249f158c8742158cbf7feea8cd0e0d9dafa7579c
Aris Gail Mendoza <arisgailseemendoza@gmail.com>
2018-07-14 14:00:48 +0800

    Revised Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS295.S37
-Project - Partial Course Requirement for CS295.S37 (Special Topics on Applications In Medicine And Public Health Informatics)
---
+++ app/src/main/java/edu/admu/cs295s37/watchout/MyApplication.java
@@ -0,0 +1,11 @@
+package edu.admu.cs295s37.watchout;
+
+import android.app.Application;
+import io.realm.Realm;
+
+public class MyApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
