04f6bb3ed89c482610f68a86d79c5147895dc866
guilletron <jerwin.guillermo@gmail.com>
2018-07-14 14:51:01 +0800

    Login activity layout done

--- app/src/main/java/edu/admu/cs295s37/watchout/LoginActivity.java
+++ app/src/main/java/edu/admu/cs295s37/watchout/LoginActivity.java
@@ -2,11 +2,26 @@ package edu.admu.cs295s37.watchout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity
 class LoginActivity extends AppCompatActivity {
+
+    @ViewById
+    EditText etEmail;
+    @ViewById
+    EditText etPassword;
+    @ViewById
+    CheckBox cbRememberMe;
+    @ViewById
+    Button bSignIn;
+    @ViewById
+    Button bSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
